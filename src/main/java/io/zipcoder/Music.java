d58f76ffe87957543b75ded7d43e2e6c55f58480
dbond <debon1426@gmail.com>
2020-02-15 14:06:33 -0500

    finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,44 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        int forward = searchForward(startIndex, selection);
+        int backward = searchBackward(startIndex, selection);
+
+        if (forward == 0) {
+            return backward;
+        } else if (backward == 0)
+            return forward;
+
+        if (forward < backward ) {
+            return forward;
+        } else
+            return backward;
+    }
+
+    private Integer searchForward(Integer startIndex, String selection){
+        int songIndex = 0;
+        for (int i = 0; i <= playList.length-1; i++ ){
+            if (selection == playList[i] ){
+                songIndex = i;
+                break;
+            }
+        }
+         return Math.abs(startIndex - songIndex)%playList.length;
+    }
+
+    private Integer searchBackward(Integer startIndex, String selection){
+        int songIndex = 0;
+        for (int i = playList.length-1; i >= 0; i-- ){
+            if (selection == playList[i] ){
+                songIndex = i;
+                break;
+            }
+        }
+        return playList.length%Math.abs(startIndex - songIndex);
     }
 }
